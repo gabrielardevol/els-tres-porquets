76c5fffd3daf0a386b8c738746facb613734157b
Gabriel Ardevol <artsdevol@gmail.com>
2022-11-27 17:30:53 +0100

    drama

--- view/test.rb
+++ view/test.rb
@@ -1,36 +1,29 @@
-time1 = Time.now
+p time1 = Time.now
 # sleep(5)
-time2 = nil
-timelapse = 15
-count = 0
-start_distance = 0
-mid_distance = 49
-final_distance = 100
-wolf_run = timelapse.to_f / (mid_distance.to_f + final_distance.to_f)
-
-p wolf_run
-
+p time2 = nil
+p timelapse = 15
+p count = 0
+p start_distance = 0
+p mid_distance = 49
+p final_distance_init = 100
+p "*" * 15
+p wolf_run = timelapse / (mid_distance + final_distance_init).to_f
 
 until time2.to_f - time1.to_f >= timelapse do
   time2 = Time.now
-  puts ("🌲" * start_distance) + "🐺" + "🌲" * (mid_distance + count) + "🐷" + "🌲" * final_distance + "🏠"
+  # puts ("🌲" * start_distance.to_i) + "🐺" + "🌲" * (mid_distance + count).to_i + "🐷" + "🌲" * final_distance_init + "🏠"
   puts "press any key to run to the house!"
   gets.chomp ? count += 1 : nil
   p now = time2.to_f - time1.to_f
   p wolf_run
-  p now / wolf_run
+  p (now / wolf_run).to_i
 
-  if 2 + 2 == 4
-    p "wolf"
-  else
-    nil
-  end
 
   #if now >= wolf_run.to_f do
   #  puts "WOLF"
   #  wolf_run += wolf_run
   #end
-  start_distance += 1
-  mid_distance -= 1
-  final_distance -= 1
+  p start_distance = (now / wolf_run)
+  p mid_distance -= mid_distance - (now / wolf_run)
+  p final_distance = final_distance_init - (now / wolf_run)
 end
--- view/test2.rb
+++ view/test2.rb
@@ -1,1 +1,36 @@
-puts "🐺🥯  🦆 🦉 🥓 🦄 🦀 🖕 🍣 🍤 🍥 🍡 🥃 🥞 🤯 🤪 🤬 🤮 🤫 🤭 "
+  time1 = Time.now
+
+  start_distance = 0
+  wolf_to_pig_distance = 10
+  count = 0
+  final_distance = 90
+  total_distance = wolf_to_pig_distance + final_distance
+# sleep(4)
+  total_time = 15
+  p time_passed = (Time.now - time1)
+  p per_second = (100.0 / total_time)
+  p per_second * time_passed
+  final_distance = 90
+ sleep(0.9)
+  p time_passed = (Time.now - time1)
+  wolf_runs = per_second * time_passed
+
+      p start_distance.to_i
+      p "wolf runs #{wolf_runs}"
+      p wolf_to_pig_distance.to_i
+  puts ("🌲" * start_distance.to_i) + "🐺" + "🌲" * (wolf_to_pig_distance + count).to_i + "🐷" + "🌲" * final_distance + "🏠"
+  #def wolf_run
+    if wolf_runs <= wolf_to_pig_distance
+      start_distance += wolf_runs
+      wolf_to_pig_distance -= wolf_runs
+    else
+      wolf_to_pig_distance = 0
+      start_distance = total_distance - final_distance
+    end
+  #end
+        p start_distance.to_i
+        p "wolf runs #{wolf_runs}"
+        p wolf_to_pig_distance.to_i
+
+
+  puts ("🌲" * start_distance.to_i) + "🐺" + "🌲" * (wolf_to_pig_distance + count).to_i + "🐷" + "🌲" * final_distance + "🏠"
