7e744d97bfce45f0a687dfffded87cb970057126
Gabriel Ardevol <artsdevol@gmail.com>
2022-11-28 14:57:01 +0100

    app.rb has good style now

--- view/app.rb
+++ view/app.rb
@@ -1,14 +1,14 @@
-require_relative "game_controller.rb"
-require_relative "text_display_view.rb"
-require_relative "image_repository.rb"
+require_relative 'game_controller'
+require_relative 'text_display_view'
+require_relative 'image_repository'
 
 image_repository = ImageRepository.new
 
-Text.new.display("Once upon a time, pig family lives happily in middle of forest...", 0.07, 100, "", 1)
+Text.new.display('Once upon a time, pig family lives happily in middle of forest...', 0.07, 100, '', 1)
 sleep(1.5)
-Text.new.display("🐷 🐷 🐷 🐷", 0.3, 100, "Once upon a time, pig family lives happily in middle of forest...")
+Text.new.display('🐷 🐷 🐷 🐷', 0.3, 100, 'Once upon a time, pig family lives happily in middle of forest...')
 gets.chomp
-Text.new.display("Problem is... Mother pig ran out of money", 0.07, 100)
+Text.new.display('Problem is... Mother pig ran out of money', 0.07, 100)
 
 image_repository.image1
 
