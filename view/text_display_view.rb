1fea6713f78515161ca88e06d6146ae0474cf4d7
Gabriel Ardevol <artsdevol@gmail.com>
2022-11-28 15:02:21 +0100

    text_display_view.rb has correct style now

--- view/text_display_view.rb
+++ view/text_display_view.rb
@@ -1,17 +1,27 @@
+# Text View displays strings using visual effects on console
 class Text
-  def display(array, time, top_spacing, upper_string = "", bottom_spacing = 0)
+  def display(array, time, top_space, upper_string = '', bottom_space = 0)
     index = 0
-    until index == array.size do
-      top_spacing.times do
-        puts ""
-      end
+    until index == array.size
+      top_spacing(top_space)
       puts upper_string
       puts array[0..index]
-      bottom_spacing.times do
-        puts ""
-      end
+      bottom_spacing(bottom_space)
+
       index += 1
       sleep(time)
+    end
+  end
+
+  def top_spacing(top_space)
+    top_space.times do
+      puts ''
+    end
+  end
+
+  def bottom_spacing(bottom_space)
+    bottom_space.times do
+      puts ''
     end
   end
 end
