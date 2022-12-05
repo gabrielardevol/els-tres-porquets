b27576a84d1f8f22b492e62480b36168ea3cea0b
Gabriel Ardevol <artsdevol@gmail.com>
2022-12-05 08:59:01 +0100

    adding margin to house view

--- view/house_game_view.rb
+++ view/house_game_view.rb
@@ -11,31 +11,31 @@ class HouseGameView < GameView
     row2 = "🧱🧱    🧱🧱"
     row1 = "🧱🧱🐷🐷🧱🧱"
 
-    margin= "margin"
+    @margin= "margin"
 
 
-    puts n >= 23 ? ("#{margin} #{row6}") : nil
-    puts n >= 22 ? ("#{margin} #{row5}") : nil
+    puts n >= 23 ? ("#{@margin} #{row6}") : nil
+    puts n >= 22 ? ("#{@margin} #{row5}") : nil
     n > 19 && n < 22 ? puts( " "*(9-((n-19)*2)) + "🧱"*(n - 19)) : nil
-    puts n >= 19 ? ("#{margin} #{row4}") : nil
+    puts n >= 19 ? ("#{@margin} #{row4}") : nil
     n > 14 && n < 19 ? puts( " "*(11-((n-14)*2)) + "🧱"*(n - 14)) : nil
-    puts n >= 14 ? ("#{margin} #{row3}") : nil
+    puts n >= 14 ? ("#{@margin} #{row3}") : nil
     n > 8 && n < 14 ? puts( " "*(12-((n-8)*2)) + "🧱"*(n - 8)) : nil
     # n > 8 ? fractioned_row(n) : nil
-    puts n >= 8 ? ("#{margin} #{row2}") : nil
+    puts n >= 8 ? ("#{@margin} #{row2}") : nil
     n > 4 && n < 8 ? fractioned_row(n) : nil
-    puts n >= 4 ? ("#{margin} #{row1}") : nil
+    puts n >= 4 ? ("#{@margin} #{row1}") : nil
     n < 4 ? fractioned_row(n) : nil
 
   end
 
   def fractioned_row(n)
-    n == 7 ? puts("  🧱    🧱🧱") : nil
-    n == 6 ? puts("        🧱🧱") : nil
-    n == 5 ? puts("          🧱") : nil
-    n == 3 ? puts("  🧱🐷🐷🧱🧱") : nil
-    n == 2 ? puts("    🐷🐷🧱🧱") : nil
-    n == 1 ? puts("    🐷🐷  🧱") : nil
-    n == 0 ? puts("    🐷🐷    ") : nil
+    n == 7 ? puts("#{@margin}   🧱    🧱🧱") : nil
+    n == 6 ? puts("#{@margin}         🧱🧱") : nil
+    n == 5 ? puts("#{@margin}           🧱") : nil
+    n == 3 ? puts("#{@margin}   🧱🐷🐷🧱🧱") : nil
+    n == 2 ? puts("#{@margin}     🐷🐷🧱🧱") : nil
+    n == 1 ? puts("#{@margin}     🐷🐷  🧱") : nil
+    n == 0 ? puts("#{@margin}     🐷🐷    ") : nil
   end
 end
