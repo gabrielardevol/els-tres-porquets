4b981c0980702b2adb6488ce86e0fb2e3262527a
Gabriel Ardevol <artsdevol@gmail.com>
2022-12-05 08:26:49 +0100

    implementing yield

--- view/yield_test.rb
+++ view/yield_test.rb
@@ -1,17 +1,18 @@
 class YieldTest
- def test
-  def one_yield
-    yield
-  end
 
-  def multiple_yields
-    yield
-    yield
-  end
+ # def test
+    def one_yield
+      yield
+    end
 
-  one_yield { puts "one yield" }
-  multiple_yields { puts "multiple yields" }
-end
+    def multiple_yields
+      yield
+      yield
+    end
+
+    one_yield { puts "one yield" }
+    multiple_yields { puts "multiple yields" }
+ # end
 end
 
 YieldTest.new.test
