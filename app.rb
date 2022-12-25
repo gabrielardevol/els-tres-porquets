9a093e4a498b35ff98c405576425687dd7318a8f
Gabriel Ardevol <artsdevol@gmail.com>
2022-12-25 21:12:21 +0100

    okay...

--- app.rb
+++ app.rb
@@ -1,8 +1,5 @@
 require_relative 'hiding_game/hiding_game_controller'
-require_relative 'text_display_view'
-require_relative 'image_repository'
-
-image_repository = ImageRepository.new
+require_relative 'text_display'
 
 #Text.new.display('Once upon a time, pig family lives happily in middle of forest...', 0.07, 100, '', 1)
 #sleep(1.5)
